12ebcb78d63852d36949d9f1f2f5186c5ea70031
David Renshaw <dwrenshaw@gmail.com>
2012-04-21 17:58:29 -0400

    new way to render the world

--- game.js
+++ game.js
@@ -33,16 +33,20 @@
 
 
 
- var worldwidth = 200
- var worldheight = 100
+ var worldrows = 100
+ var worldcolumns = 200
 
- var world = new Array(worldwidth)
- for (i = 0; i < world.length; i++) {
-     world[i] = new Array(worldheight)
+ var world = new Array(worldrows)
+ for (var ii = 0; ii < world.length; ++ii) {
+     world[ii] = new Array(worldcolumns)
  }
 
- function addToWorld(stringarray, x, y) {
-     true
+ function addToWorld(stringarray, row, col) {
+     for(var i = 0; i < stringarray.length; ++i){
+         for(var j = 0; j < stringarray[i].length; ++j) {
+             world[i + row][j + col] = stringarray[i].charAt(j)
+         }
+     }
  }
 
  addToWorld(new Array("WWWWWWWWWW",
@@ -54,30 +58,38 @@
                       "W      W W",
                       "W      W W",
                       "WWwWWWWWWW"),
-            220,
-            120)
+            55,
+            105)
  
 
- var viewportwidth = 50
- var viewportheight = 30
+ var viewportrows = 30;
+ var viewportcolumns = 50;
 
- var viewportx = 100
- var viewporty = 50
+ var viewporti = 50;
+ var viewportj = 100;
 
- var playerx = 110
- var playery = 60
+ var playeri = 60;
+ var playerj = 110;
+
+ function subarrayToString(arr, start, end) {
+     var res = "";
+     for (var j = start; j < end; ++j) {
+         if (arr[j] !== undefined) {
+             res += arr[j];
+         } else {
+             res += '&nbsp'
+         }
+     }
+
+     return res.replace(/ /g,'&nbsp');
+ }
 
  function render() {
    var e;
-   for (i = 0; i < viewportheight; i++) {
-     e = document.getElementById('line' + i);
-     e.innerHTML = lines[i].replace(/ /g,'&nbsp');
+   for (var i = 0; i < viewportrows; ++i) {
+       e = document.getElementById('line' + i);
+       e.innerHTML = subarrayToString(world[i + viewporti], viewportj, viewportj + viewportcolumns)
    }
-
-    e = document.getElementById('topbar' );
-    e.innerHTML = "+---------------------------------------------------+";
-    e = document.getElementById('botbar' );
-    e.innerHTML = "+---------------------------------------------------+";
 
  }
 
